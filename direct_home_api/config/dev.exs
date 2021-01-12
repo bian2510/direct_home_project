3cd04136416e1d7231632627e56b45a5e2077f32
Fabian Hernandez <bian251091@gmail.com>
2021-01-12 16:04:16 -0300

    add phx app to docker-compose

--- direct_home_api/config/dev.exs
+++ direct_home_api/config/dev.exs
@@ -5,7 +5,7 @@ config :direct_home_api, DirectHomeApi.Repo,
   username: "postgres",
   password: "postgres",
   database: "direct_home_api_dev",
-  hostname: "localhost",
+  hostname: "db",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
