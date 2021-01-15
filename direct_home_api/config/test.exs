7542599c2b32f2a0af3771d62a05b7d8c50a49bf
Hosward Villoria <hosward.avc@gmail.com>
2021-01-15 18:31:45 -0300

    changes hostname

--- direct_home_api/config/test.exs
+++ direct_home_api/config/test.exs
@@ -9,7 +9,7 @@ config :direct_home_api, DirectHomeApi.Repo,
   username: "postgres",
   password: "postgres",
   database: "direct_home_api_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
